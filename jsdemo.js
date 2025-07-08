2bc53fb0a18245a51189fec0c799dce807f99a58
Fax <6yhace1978@gmail.com>
2025-07-08 16:44:20 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -2,3 +2,11 @@ console.log("This output is from the external JS file");
 console.log("Name: Jordan Parker");
 console.log("School: Julius L. Chambers");
 console.log("Grade: 11th");
+
+var name = "Jordan Parker";
+let school = "Julius L. Chambers";
+var grade = "11th";
+
+document.getElementById("name").innerHTML = "Name: <b>" + name + "</b>";
+document.getElementById("name").innerHTML = "Grade: <b>" + grade + "</b>";
+document.getElementById("name").innerHTML = "School: <b>" + school + "</b>";
